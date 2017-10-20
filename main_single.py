0ca31e9ce2f12d5218368afdc6091fe3d3d7ad8f
kabramova <lumina.noctis@gmail.com>
2017-10-20 12:22:37 +0200

    Fix mutation variance and crossover probability; left motor activation back to negative number; direct tracker's velocity back to sum

--- main_single.py
+++ main_single.py
@@ -12,7 +12,7 @@ import shutil
 # @profile(print_stats=10, dump_stats=True)
 
 
-def main(agent_type, seed_num, mutation_variance, prob_crossover):
+def main(agent_type, seed_num):
     # load configuration settings
     json_data = open('config.json')
     config = json.load(json_data)
@@ -32,11 +32,6 @@ def main(agent_type, seed_num, mutation_variance, prob_crossover):
         config['agent_params']["n_audio_connections"] = 1
         # config['agent_params']["n_effector_connections"] = 1
         config['agent_params']["n_effector_connections"] = 2
-
-    if mutation_variance:
-        config['evolution_params']['mutation_variance'] = mutation_variance
-    if prob_crossover:
-        config['evolution_params']['prob_crossover'] = prob_crossover
 
     # set up evolution
     evolution = Evolution(config['evolution_params']['pop_size'],
@@ -67,7 +62,5 @@ if __name__ == '__main__':
     parser.add_argument("agent_type", type=str, help="specify the type of the agent you want to run",
                         choices=["buttons", "direct"])
     parser.add_argument("seed_num", type=int, help="specify random seed number")
-    parser.add_argument("-m", "--mutation_variance", type=int, default=1, help="specify the mutation variance")
-    parser.add_argument("-c", "--prob_crossover", type=int, default=0.8, help="specify the probability of crossover")
     args = parser.parse_args()
-    main(args.agent_type, args.seed_num, args.mutation_variance, args.prob_crossover)
+    main(args.agent_type, args.seed_num)
